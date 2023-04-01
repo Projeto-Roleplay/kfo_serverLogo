2467913de2caff36347451a5b3199b2b8f121831
Murilo Bada <murilomaffiolettibada@hotmail.com>
2023-04-01 13:18:01 -0300

    added addChatSuggestion

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,20 +1,31 @@
 RedEM = exports["redem_roleplay"]:RedEM()
 local display = false
 
-RegisterNetEvent('kfo_serverLogo:nui')
-AddEventHandler('kfo_serverLogo:nui', function()
-    display = not display
-    SetNuiFocus(display, false)
-    SendNUIMessage({
-        type = "ui",
-        display = display
-    })
+RegisterCommand('logo', function(source, args)
+    if args[1] then
+        local pos = string.lower(args[1])
+
+        if pos ~= '' then
+            
+        end
+
+    else
+        TriggerEvent("redem_roleplay:Tip", 'Invalid position inserted', 5000)
+    end
 end)
 
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
-        display = false
-        SetNuiFocus(false, false)
+        addChatSuggestion()
+        SendNUIMessage({
+            type = "ui",
+            display = true
+        })
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
         SendNUIMessage({
             type = "ui",
             display = false
--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,5 +1,1 @@
 RedEM = exports["redem_roleplay"]:RedEM()
-
-RegisterCommand('thombstone', function(source)
-    TriggerClientEvent('kfo_serverLogo:nui', source )
-end)
